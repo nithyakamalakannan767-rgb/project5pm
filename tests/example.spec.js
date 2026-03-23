92be5c20982e331b8a70b4ee8609fa89d626f3f0
nithyakamalakannan767 <nithyakamalakannan767@gmail.com>
2026-03-23 14:05:32 +0530

    add fileadd

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,6 +2,6 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://amazon.com');
+  await page.goto('https://www.flipkart.com/');
 
  });
